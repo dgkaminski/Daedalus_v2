9410d12efc8a88bfb441908c91e9c116723d8408
dgkaminski <dgkaminski@wpi.edu>
2022-04-03 17:08:19 -0400

    Initial commit

---
+++ Assets/VR Walking Running Jumping/Scripts/Platform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public enum TypeOfMovement { vertical, horizontal};
+    public TypeOfMovement movement;
+
+    public float speed=0.5f;
+    public float amplitude;
+
+    public Vector3 originPos;
+
+
+    void Start()
+    {
+        originPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (movement == TypeOfMovement.horizontal)
+        {
+            transform.position= originPos+new Vector3(amplitude*Mathf.Sin(speed*Time.fixedTime),0,0);
+        }
+       
+        if(movement==TypeOfMovement.vertical)
+        {
+            transform.position=originPos + new Vector3(0, amplitude*Mathf.Sin(speed * Time.fixedTime), 0);
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Daedalus_v2
+ A VR game developed in 2022 at MAMS for the Apps 4 Good program that places the user in a maze from which they must escape with the help of a teammate
